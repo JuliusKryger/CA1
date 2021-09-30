7c52133ae692b9ef9ea2bc027a18df490053a7f8
AlexKenfelt <cph-ak428@cphbusiness.dk>
2021-09-30 10:58:21 +0200

    HobbieList

--- src/main/java/facades/InterfaceHobbies.java
+++ src/main/java/facades/InterfaceHobbies.java
@@ -2,8 +2,10 @@ package facades;
 
 import dtos.HobbiesDTO;
 import dtos.PersonDTO;
+import entities.Person;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface InterfaceHobbies {
 
