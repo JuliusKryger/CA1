3cc8d7e813ff2aa4227c1c3d03659973962bd0e6
Julius Madsen <2330051117@protonmail.com>
2021-09-30 11:21:36 +0200

    person entity corrections

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -1,12 +1,7 @@
 package entities;
 
-import dtos.*;
-
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
 @NamedQueries({
@@ -22,79 +17,7 @@ public class Person implements Serializable {
     private Integer id;
     private String firstName;
     private String lastName;
-    private String phoneNumber;
-    private String email;
-    private int age;
 
 
-/*    @Transient
-    private AddressDTO addressDTO;
-    private HobbiesListDTO hobbiesListDTO;
 
- */
-
-    public Person(){
-
-    }
-
-    public Person(String firstName, String lastName, String phoneNumber, String email, int age, AddressDTO addressDTO, HobbiesListDTO hobbies){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.age = age;
-
-        /* TODO: Ingen DTO's i entities.
-        this.addressDTO = addressDTO;
-        this.hobbiesListDTO = hobbies;
-         */
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
